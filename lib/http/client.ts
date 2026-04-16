fca067ea06fb0b54a330b376ea9acc2dae5a07e6
Annyotamo <abprogresss@gmail.com>
2026-04-16 12:45:10 +0530

    changed middleware.ts to proxy.ts

--- lib/http/client.ts
+++ lib/http/client.ts
@@ -1,11 +1,7 @@
 import axios from "axios";
 import { getAuthToken } from "@/lib/auth/token";
 
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
-
-if (!baseURL) {
-    throw new Error("NEXT_PUBLIC_BASE_URL is not defined.");
-}
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL ?? "";
 
 const defaultConfig = {
     baseURL,

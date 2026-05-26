da851dd8868e7a944b91c6df0d1f5f4c05f35b7e
Annyotamo <abprogresss@gmail.com>
2026-05-26 23:48:18 +0530

    Added changes

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -1,6 +1,6 @@
 // packages/shared/index.ts
 
-const RAW_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://api.greenledgeresg.com/v1";
+const RAW_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://dev.greenledgeresg.com";
 export const API_BASE_URL = RAW_BASE_URL.replace(/\/+$/, "");
 console.log("API_BASE_URL", API_BASE_URL);
 export const buildUrl = (path: string) => {

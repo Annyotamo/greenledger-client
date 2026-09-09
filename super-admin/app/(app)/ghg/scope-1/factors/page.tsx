f8be86ec1c2c5d8a03d64fab0fa01e3c3fb10ded
Annyotamo <abprogresss@gmail.com>
2026-09-09 13:55:25 +0530

    Added changes

--- super-admin/app/(app)/ghg/scope-1/factors/page.tsx
+++
@@ -1,6 +0,0 @@
-import { AddFactorForm } from "./AddFactorForm";
-
-export default function FactorsPage() {
-  return <AddFactorForm />;
-}
-
--- super-admin/app/(app)/ghg/scope-2/factors/page.tsx
+++
@@ -1,6 +0,0 @@
-import { AddScope2VersionForm } from "./AddScope2VersionForm";
-
-export default function Scope2FactorsPage() {
-  return <AddScope2VersionForm />;
-}
-
